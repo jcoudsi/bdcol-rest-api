dad44b75cbce993deabfb091432631fc6dab55c8
Julien Coudsi <jcoudsi@gmail.com>
2015-01-14 21:33:27 +0100

    Initial commit

---
+++ bdcolrestapi/app.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var mongoose = require('mongoose')
+var app = express()
+
+
+var server = app.listen(8080, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
+
+app.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({test:2}));
+})
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+})
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+})
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+})
